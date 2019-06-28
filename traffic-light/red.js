2c96219ce837abbe477fc4e8035764638c8628ba
A.Paleev <paleev045@gmail.com>
2019-06-28 17:18:25 +0300

    внес изменения в проект "светофор"

--- traffic-light/red.js
+++ traffic-light/red.js
@@ -2,16 +2,16 @@ function red() {
 	document.getElementById('red').style.background = 'red';
 	document.getElementById('yellow').style.background = 'black';
 	document.getElementById('green').style.background = 'black';
-	setTimeout(color1, 2000);
+	setTimeout(color1, 1500);
 }
 function color1() {
 	document.getElementById('yellow').style.background = 'yellow';
 	document.getElementById('red').style.background = 'black';
-	setTimeout(color2, 2000);
+	setTimeout(color2, 1500);
 }
 function color2() {
 	document.getElementById('yellow').style.background = 'black';
 	document.getElementById('green').style.background = 'green';
-	setTimeout(red, 2000);
+	setTimeout(red, 1500);
 }
 window.onload = red();
